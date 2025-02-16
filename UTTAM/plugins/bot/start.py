1e7bbe9e5db4c214ec4e1f370f012e1016292bac
Badnam xd <yadavxd746@gmail.com>
2025-02-17 00:28:42 +0530

    Update start.py

--- UTTAM/plugins/bot/start.py
+++ UTTAM/plugins/bot/start.py
@@ -9,19 +9,19 @@ from pyrogram import *
 from pyrogram.types import * 
 
 PHONE_NUMBER_TEXT = (
-    """**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ─────•\n┆⚘ ʜᴇʏ, ɪ ᴀᴍ : [˹ 🅤sᴇʀʙᴏᴛ ˼](t.me/USERBOT_ROBOT)\n┆⚘ ᴍᴏʀᴇ ᴀɴɪᴍᴀᴛɪᴏɴ,ғᴜɴ\n┊⚘ ᴘᴏᴡᴇʀғᴜʟ & ᴜsᴇғᴜʟ ᴜsᴇʀʙᴏᴛ\n╰─────────────────────•\n❍ ʜσᴡ ᴛσ υsє ᴛʜɪs ʙσᴛ - [ᴛɪᴘs ʜᴇʀᴇ](https://t.me/RADHE_ALLBOT) \n❍ sᴛꝛɪηɢ sєᴄᴛɪση ʙσᴛ ⁚ [sᴇssɪᴏɴ-ʙᴏᴛ](https://t.me/STRING_SESSION_GENN_BOT) \n•──────────────────────•\n❍ ᴄʟσηє ⁚ /clone [ sᴛʀɪɴɢ sᴇssɪᴏɴ ]\n•──────────────────────•\n❍ ᴘσɯҽɾҽᴅ ʙу ⏤‌‌‌‌  [𝙱𝙾𝚃-𝙲𝙷𝙰𝙼𝙱𝙴𝚁](https://t.me/ll_BOTCHAMBER_ll) \n•──────────────────────•**"""
+    """**╭────── ˹ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ˼ ─────•\n┆⚘ ʜᴇʏ, ɪ ᴀᴍ : [˹ 🅤sᴇʀʙᴏᴛ ˼](t.me/Badnam_op_music_bot)\n┆⚘ ᴍᴏʀᴇ ᴀɴɪᴍᴀᴛɪᴏɴ,ғᴜɴ\n┊⚘ ᴘᴏᴡᴇʀғᴜʟ & ᴜsᴇғᴜʟ ᴜsᴇʀʙᴏᴛ\n╰─────────────────────•\n❍ ʜσᴡ ᴛσ υsє ᴛʜɪs ʙσᴛ - [ᴛɪᴘs ʜᴇʀᴇ](https://t.me/ABOUT_BADNAM_YADAV_XD) \n❍ sᴛꝛɪηɢ sєᴄᴛɪση ʙσᴛ ⁚ [sᴇssɪᴏɴ-ʙᴏᴛ](https://t.me/Jio_savan_music_bot) \n•──────────────────────•\n❍ ᴄʟσηє ⁚ /clone [ sᴛʀɪɴɢ sᴇssɪᴏɴ ]\n•──────────────────────•\n❍ ᴘσɯҽɾҽᴅ ʙу ⏤‌‌‌‌  [𝐁ᴀᴅɴᴀᴍ 𝐁ᴀʙʏ](https://t.me/ABOUT_BADNAM_YADAV_XD) \n•──────────────────────•**"""
 )
 
 @app.on_message(filters.command("start"))
 async def hello(client: app, message):
     buttons = [
            [
-                InlineKeyboardButton("˹ ᴏᴡɴᴇʀ ˼", url="https://t.me/TEAM_INDIANS_BOT"),
-                InlineKeyboardButton("˹ ᴜᴘᴅᴀᴛᴇ ˼", url="https://t.me/ll_BOTCHAMBER_ll"),
+                InlineKeyboardButton("˹ ᴏᴡɴᴇʀ ˼", url="https://t.me/+o-JWMWgnMYw4ZjRl"),
+                InlineKeyboardButton("˹ ᴜᴘᴅᴀᴛᴇ ˼", url="https://t.me/+o-JWMWgnMYw4ZjRl"),
             ],
             [
-                InlineKeyboardButton("˹ sᴜᴘᴘᴏʀᴛ ˼", url="https://t.me/BOT_SUPPORT_GROUP7"),
-                InlineKeyboardButton("˹ ᴍᴜsɪᴄ ˼", url="https://t.me/sanataniiMusicBot"),
+                InlineKeyboardButton("˹ sᴜᴘᴘᴏʀᴛ ˼", url="https://t.me/+o-JWMWgnMYw4ZjRl"),
+                InlineKeyboardButton("˹ ᴍᴜsɪᴄ ˼", url="https://t.me/+o-JWMWgnMYw4ZjRl"),
             ],
             ]
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -34,7 +34,7 @@ async def clone(bot: app, msg: Message):
     cmd = msg.command
     phone = msg.command[1]
     try:
-        await text.edit("🎨 ᴘʀᴏᴄᴇssɪɴɢ.....✲")
+        await text.edit("🎨 ʙᴀᴅɴᴀᴍ ᴋᴏ ᴘᴀᴘᴀ ʙᴏʟ ᴘᴇʜʟᴇ.....✲")
                    # change this Directry according to ur repo
         client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="UTTAM/plugins"))
         await client.start()
