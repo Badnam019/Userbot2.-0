87223036d18a61ccc342e887cdeeb7a761496544
Badnam xd <yadavxd746@gmail.com>
2025-02-17 00:20:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "20054364")) #optional
 API_HASH = getenv("API_HASH", "6a9a4fbda55762023c32133f76cbc1d1") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5738579437").split()))
-OWNER_ID = int(getenv("OWNER_ID", "8093150680"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8037151602").split()))
+OWNER_ID = int(getenv("OWNER_ID", "8037151602"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://envs.sh/620.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/351zel.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP","-1002458103093")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/papahuu/radhexuserbot")
+REPO_URL = getenv("REPO_URL", "https://github.com/Badnam019/Userbot2.-0")
 BRANCH = getenv("BRANCH", "main") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
